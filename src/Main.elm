a7ac0b53bcc76a8863e40c1c7b4197041251593a
Kris Sugandhi <kris.sugandhi@gmail.com>
2022-02-22 22:20:25 +1100

    Render new game button

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,41 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-main : Html.Html msg
+-- MAIN
 main =
-  text "Hello World!"
+  Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+
+type alias AppData =
+  { deckId : String }
+
+type alias Model = AppData
+
+init : Model
+init =
+  { deckId = "" }
+
+
+-- UPDATE
+
+type Msg = NewGame
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    NewGame ->
+      { model | deckId = "[TODO] Shuffle decks and get Deck ID" }
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick NewGame ] [ text "New Game" ]
+    , div [] [ text (model.deckId) ]
+    ]
